cec4736a792602567a1e1d695e558afdf396753e
godwinchibyk <godwin.chibyk@gmail.com>
2024-06-25 12:34:03 +0100

    complete basic setup

---
+++ inertia/app/app.tsx
@@ -0,0 +1,25 @@
+/// <reference path="../../adonisrc.ts" />
+/// <reference path="../../config/inertia.ts" />
+
+import '../../resources/css/app.css'
+import '../../resources/css/data-tables-css.css'
+import '../../resources/css/satoshi.css'
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react'
+import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+
+const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+
+createInertiaApp({
+  progress: { color: '#5468FF' },
+
+  title: (title) => `${title} - ${appName}`,
+
+  resolve: (name) => {
+    return resolvePageComponent(`../pages/${name}.tsx`, import.meta.glob('../pages/**/*.tsx'))
+  },
+
+  setup({ el, App, props }) {
+    createRoot(el).render(<App {...props} />)
+  },
+})
